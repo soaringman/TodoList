916e940db9f3435cf456319162fcff11ce532c94
Alexey Gulyaev <sargonmeister@gmail.com>
2018-03-28 13:20:52 +0300

    add Label and any info in todoDetailsViewController

--- TodoList/TodoDetailsViewController.swift
+++ TodoList/TodoDetailsViewController.swift
@@ -12,10 +12,23 @@ class TodoDetailsViewController: UIViewController {
     
     private let todoItem: TodoItem
     private let todoDataProvider: TodoDataProvider
+    /*
+    //Писал все в комитах потому что не понимаю как можно проинициализировать
+    //Создам переменнную для того что бы в дальнейшем использовать ее для описания вьюхи размером на весь свободный TodoDetailsViewController
+    private let todoDetailsView: UIView!
+     
+    //добавляю новые переменные (типа UILabel для того что бы в них в дальнейшем в них положить содержимое TodoItem соответствующих полей )
+    private var todoLabeldateTime: UILabel
+    private var todoLabelTheme: UILabel
+    private var todoLabelText: UILabel
+    private var todoImageisCompleted: UIImage
     
+    */
     init(todoItem: TodoItem, todoDataProvider: TodoDataProvider) {
         self.todoItem = todoItem
         self.todoDataProvider = todoDataProvider
+        
+    
         
         super.init(nibName: nil, bundle: nil)
     }
@@ -29,5 +42,29 @@ class TodoDetailsViewController: UIViewController {
         
         navigationItem.title = todoItem.text
         view.backgroundColor = UIColor.white
+        /*
+         todoDetailsView = UIView(frame: CGRect.zero)
+         
+         //Добавляем наши поля
+         todoLabeldateTime = todoItem.dateTime
+         todoLabelTheme = todoItem.theme
+         todoLabelText = todoItem.text
+         
+         //есть сомнения по поводу правильности ниженаписанного так как isComleted булево значение
+         todoImageisCompleted = todoItem.isCompleted
+         
+         //добавляю мою вьюху как Subview
+         view.addSubview(todoDetailsView)
+         
+         //Использую для того что бы расширить DetailsView на весь экран
+         override func viewDidLayoutSubviews() {
+         super.viewDidLayoutSubviews()
+         
+         todoDetailsView.frame = view.bounds
+         }
+         //Надо задать отступы для полей (todoLabeldateTime = todoItem,todoLabelTheme, todoLabelText)
+         //
+ */
+    
     }
 }
