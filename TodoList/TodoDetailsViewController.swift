b379b21232a7cfbe81d00aae893d17eb10c3a990
Alexey Gulyaev <ghost-shadows@yandex.ru>
2018-03-31 16:06:18 +0300

    add  buttons

--- TodoList/TodoDetailsViewController.swift
+++ TodoList/TodoDetailsViewController.swift
@@ -18,6 +18,14 @@ class TodoDetailsViewController: UIViewController {
     private var dateTimeLabel: UILabel
     private var themeLabel: UILabel
     private var textLabel: UILabel
+    
+    //задание переменных под кнопки
+    private var isCompletedButton: UIButton
+    private var isNotCompletedButton: UIButton
+    //задание переменных под картинки для кнопок
+    private var isCompletedButtonImage: UIImage!
+    private var isNotCompletedButtonImage: UIImage!
+    private let buttonSize: CGFloat = 50.0
 
     
     // MARK: Init
@@ -30,6 +38,11 @@ class TodoDetailsViewController: UIViewController {
         dateTimeLabel = UILabel(frame: .zero)
         themeLabel = UILabel(frame: .zero)
         textLabel = UILabel(frame: .zero)
+        
+        //инициализация переменных под кнопки и присваивание им значений
+        isCompletedButton = UIButton(frame: .zero)
+        isNotCompletedButton = UIButton(frame: .zero)
+     
        
         super.init(nibName: nil, bundle: nil)
     }
@@ -53,10 +66,39 @@ class TodoDetailsViewController: UIViewController {
         themeLabel.text = todoItem.theme
         textLabel.text = todoItem.text
         
+        //настройка кнопки isCompletedButton
+        isCompletedButtonImage = UIImage(named: "isCompletedImage")!.withRenderingMode(.alwaysOriginal)
+        isCompletedButton.setImage(isCompletedButtonImage, for: .normal)
+        isCompletedButton.contentMode = .scaleAspectFit
+//        isCompletedButton.layer.cornerRadius = buttonSize / 2
+//        isCompletedButton.layer.masksToBounds = false
+//        isCompletedButton.backgroundColor = UIColor.green
+        
+        //        работаем с тенью isCompletedButton
+        isCompletedButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+        isCompletedButton.layer.shadowOpacity = 0.1
+        isCompletedButton.layer.shadowRadius = 1
+        
+        //настройка кнопки isNotCompletedButton
+        isNotCompletedButtonImage = UIImage(named: "isNotCompletedImage")!.withRenderingMode(.alwaysOriginal)
+        isNotCompletedButton.setImage(isNotCompletedButtonImage, for: .normal)
+        isCompletedButton.contentMode = .scaleAspectFit
+        //        isCompletedButton.layer.cornerRadius = buttonSize / 2
+        //        isCompletedButton.layer.masksToBounds = false
+        //        isCompletedButton.backgroundColor = UIColor.green
+        
+        //        работаем с тенью isCompletedButton
+        isNotCompletedButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+        isNotCompletedButton.layer.shadowOpacity = 0.1
+        isNotCompletedButton.layer.shadowRadius = 1
+        
         // Добавление subviews в иерархию
         view.addSubview(dateTimeLabel)
         view.addSubview(themeLabel)
         view.addSubview(textLabel)
+        
+        view.addSubview(isCompletedButton)
+        view.addSubview(isNotCompletedButton)
     }
     
     override func viewDidLayoutSubviews() {
@@ -97,9 +139,32 @@ class TodoDetailsViewController: UIViewController {
             height: labelHeight
         )
         
+        //задание фрейма для кнопки isCompleted
+        let iscompletedButtonFrame = CGRect(
+            //x: horizontalGap,
+            x: buttonSize * 2,
+            //y: yOffset,
+            y: view.bounds.height - buttonSize,
+            width: buttonSize,
+            height: buttonSize
+         )
+        
+        //задание фрейма для кнопки isNotCompleted
+        let isNotcompletedButtonFrame = CGRect(
+            //x: horizontalGap,
+            x: view.bounds.width - (buttonSize * 2),
+            //y: yOffset,
+            y: view.bounds.height - buttonSize,
+            width: buttonSize,
+            height: buttonSize
+        )
+        
         dateTimeLabel.frame = dateFrame
         themeLabel.frame = themeFrame
         textLabel.frame = textFrame
+        
+        isCompletedButton.frame = iscompletedButtonFrame
+        isNotCompletedButton.frame = isNotcompletedButtonFrame
     }
     
     
